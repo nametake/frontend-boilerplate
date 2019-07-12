c56d40358628c172f6996740596d75af5c479e13
nametake <nametake.kyarabuki@gmail.com>
2019-07-12 14:09:25 +0900

    Added webpack-dev-server config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,17 @@
 import * as path from 'path';
 import { Configuration } from 'webpack';
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const config: Configuration = {
+const config: Configuration & { devServer?: DevServerConfiguration } = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  devtool: 'inline-source-map',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
@@ -28,6 +24,22 @@ const config: Configuration = {
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'assets', 'index.html')
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
   }
 };
 
