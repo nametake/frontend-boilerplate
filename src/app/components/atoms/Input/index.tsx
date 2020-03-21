515e56e23455283796fe0cc50fa3457c6094b9c5
nametake <nametake.kyarabuki@gmail.com>
2020-03-21 23:32:23 +0900

    Addded rule of no default export

--- src/app/components/atoms/Input/index.tsx
+++ src/app/components/atoms/Input/index.tsx
@@ -3,5 +3,3 @@ import styled from '@emotion/styled';
 export const Input = styled.input`
   display: block;
 `;
-
-export default { Input };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Input } from '@/app/components/atoms/Input';
 
 const Text = styled.div`
   color: hotpink;
 `;
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <div>
+      <Input />
       <Text>Hello World!</Text>
     </div>
   );
 };
-
-export default { App };
