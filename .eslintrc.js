27f549bfcd1ee5ca09b54653fa68177e712ee136
nametake <nametake.kyarabuki@gmail.com>
2021-08-15 12:28:24 +0900

    Fixed rc file type to js

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', '@emotion', '@typescript-eslint'],
+  rules: {
+    'no-console': ['error'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/no-relative-parent-imports': 'error',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  settings: {
+    'import/resolver': 'webpack',
+  },
+};
