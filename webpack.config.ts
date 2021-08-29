b67ed04c475dcd31fa9a7f495273a5b1ad4c9086
nametake <nametake.kyarabuki@gmail.com>
2021-08-29 09:43:21 +0900

    Fixed dev server config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
-import { Configuration, HotModuleReplacementPlugin } from 'webpack';
+import { Configuration } from 'webpack';
 /* eslint-enable */
-
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
-// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
-// const isPlugin = (plugin: unknown): plugin is WebpackPluginInstance =>
-//   Boolean(plugin);
 
 const config: Configuration = {
   mode: 'development',
@@ -19,6 +11,7 @@ const config: Configuration = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -50,17 +43,15 @@ const config: Configuration = {
     },
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'assets', 'index.html'),
     }),
-    ...(isDevelopment
-      ? [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]
-      : []),
   ],
   devServer: {
-    contentBase: './dist',
-    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    hot: true,
   },
 };
 
