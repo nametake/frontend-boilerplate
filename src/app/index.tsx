3f00f1ef7c8599652a3e66ad46d7aa078c5784bb
nametake <nametake.kyarabuki@gmail.com>
2021-08-29 23:41:18 +0900

    Added a Checkbox to app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { Input } from '@/app/components/atoms/Input';
+import { Checkbox } from '@/app/components/atoms/Checkbox';
 
 const Text = styled.div`
   color: hotpink;
 `;
 
 export function App(): JSX.Element {
+  const [checked, setChecked] = useState(false);
+  const handleChangeCheckbox = useCallback(() => {
+    setChecked((c) => !c);
+  }, []);
+
   // Async test
   useEffect(() => {
     async function p() {
@@ -18,6 +24,7 @@ export function App(): JSX.Element {
   return (
     <div>
       <Input />
+      <Checkbox checked={checked} onChange={handleChangeCheckbox} />
       <Text>Hello World!</Text>
     </div>
   );
