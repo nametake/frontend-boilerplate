3958afb92355175e98348f7d4bdca6eedf911b4b
nametake <nametake.kyarabuki@gmail.com>
2021-08-30 17:26:12 +0900

    Fixed storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import { StorybookConfig } from '@storybook/core-common';
+
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5',
   },
 };
+
+module.exports = config;
