2b836411d59a7806ee398d419026814a000939ad
nametake <nametake.kyarabuki@gmail.com>
2022-12-15 01:43:50 +0900

    Replaced webpack to vite storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,41 @@
-import path from 'path';
-import { StorybookConfig } from '@storybook/core-common';
+import { StorybookViteConfig } from '@storybook/builder-vite';
+import react from '@vitejs/plugin-react';
 
-const config: StorybookConfig = {
+const config: StorybookViteConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-vite',
   },
-  webpackFinal: async (config) => {
-    // https://github.com/storybookjs/storybook/issues/13145
-    const emotionReactEleven = path.dirname(
-      require.resolve('@emotion/react/package.json')
+  viteFinal: async (config) => {
+    config.plugins = config.plugins?.filter(
+      (plugin) =>
+        !(
+          Array.isArray(plugin) &&
+          plugin.some((p) => {
+            const pp = p as { name: string };
+            return pp.name.includes('vite:react');
+          })
+        )
     );
-    const emotionStyledEleven = path.dirname(
-      require.resolve('@emotion/styled/package.json')
-    );
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve?.alias,
-          '@emotion/core': emotionReactEleven,
-          '@emotion/styled': emotionStyledEleven,
-          'emotion-theming': emotionReactEleven,
+
+    config.plugins?.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
         },
-      },
+      })
+    );
+
+    config.esbuild = {
+      // Fixed: [vite] warning: Top-level "this" will be replaced with undefined since this file is an ECMAScript module
+      // https://github.com/vitejs/vite/issues/8644
+      logOverride: { 'this-is-undefined-in-esm': 'silent' },
     };
+
+    return config;
   },
 };
 
